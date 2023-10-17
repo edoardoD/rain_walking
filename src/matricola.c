ff90be4a5b833044023c2a4dc2b7a2db9273de5f
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2023-10-17 18:38:09 +0200

    free matrice di input + heap, malloc heap faill

--- src/matricola.c
+++ src/matricola.c
@@ -8,14 +8,25 @@
 
 #define GRAPH_INDEX(x, y, col) ((x) * (col) + (y))
 #define IS_DRY -1
-/*definizione delle strutture necessarie*/
-typedef enum
-{
-    false,
-    true
-} bool;
 
-/* struttura arco */
+
+/*min heap */
+const int NODE_UNDEF = -1;
+
+typedef struct {
+    int key;
+    double prio;
+} HeapElem;
+
+typedef struct {
+    HeapElem *heap;
+    int *pos; /* TODO: non inizializzato! Usare solo dopo una implementazione funzionante di minheap_change_prio() in cui la chiave da modificare si trova con una normale ricerca lineare sull'array heap[] */
+    int n; /* quante coppie (chiave, prio) sono effettivamente presenti nello heap */
+    int size; /* massimo numero di coppie (chiave, prio) che possono essere contenuti nello heap */
+} MinHeap;
+
+MinHeap *Q;
+
 typedef struct Edge
 {
     int src;       /* nodo sorgente */
@@ -33,6 +44,302 @@ typedef struct
     int *in_deg;  /* grado entrante dei nodi      */
     int *out_deg; /* grado uscente dei nodi       */
 } Graph;
+
+
+void minheap_print(const MinHeap *h)
+{
+    int i, j, width = 1;
+
+    assert(h != NULL);
+
+    printf("\n** Contenuto dello heap:\n\n");
+    printf("n=%d size=%d\n", h->n, h->size);
+    printf("Contenuto dell'array heap[] (stampato a livelli:\n");
+    i = 0;
+    while (i < h->n) {
+        j = 0;
+        while (j<width && i < h->n) {
+            printf("h[%2d]=(%2d, %6.2f) ", i, h->heap[i].key, h->heap[i].prio);
+            i++;
+            j++;
+        }
+        printf("\n");
+        width *= 2;
+    }
+    printf("\nContenuto dell'array pos[]:\n");
+    for (i=0; i<h->size; i++) {
+        printf("pos[%d]=%d ", i, h->pos[i]);
+    }
+    printf("\n\n** Fine contenuto dello heap\n\n");
+}
+
+void minheap_clear( MinHeap *h )
+{
+    int i;
+    assert(h != NULL);
+    for (i=0; i<h->size; i++) {
+        h->pos[i] = -1;
+    }
+    h->n = 0;
+}
+
+/* Costruisce un min-heap vuoto che può contenere al massimo
+   `size` elementi */
+MinHeap *minheap_create(int size)
+{
+    MinHeap *h = (MinHeap*)malloc(sizeof(*h)) ;
+    assert(h != NULL);
+    assert(size > 0);
+    h->size = size;
+    h->heap = (HeapElem*)malloc(size * sizeof(*(h->heap)));
+    assert(h->heap != NULL);
+    h->pos = (int*)malloc(size * sizeof(*(h->pos)));
+    assert(h->pos != NULL);
+    minheap_clear(h);
+    return h;
+}
+
+void minheap_destroy( MinHeap *h )
+{
+    assert(h != NULL);
+
+    h->n = h->size = 0;
+    free(h->heap);
+    free(h->pos);
+    free(h);
+}
+
+/* Restituisce true (nonzero) se l'indice `i` è valido per lo heap
+   `h`, false (zero) altrimenti. */
+static int valid(const MinHeap *h, int i)
+{
+    assert(h != NULL);
+
+    return ((i >= 0) && (i < h->n));
+}
+
+/* Scambia heap[i] con heap[j] */
+static void swap(MinHeap *h, int i, int j)
+{
+    HeapElem tmp;
+
+    assert(h != NULL);
+    assert(valid(h, i));
+    assert(valid(h, j));
+    assert(h->pos[h->heap[i].key] == i);
+    assert(h->pos[h->heap[j].key] == j);
+
+    tmp = h->heap[i];
+    h->heap[i] = h->heap[j];
+    h->heap[j] = tmp;
+
+    h->pos[h->heap[i].key] = i;
+    h->pos[h->heap[j].key] = j;
+}
+
+/* Restituisce l'indice del padre del nodo i */
+static int parent(const MinHeap *h, int i)
+{
+    assert(valid(h, i));
+
+    return (i+1)/2 - 1;
+}
+
+/* Restituisce l'indice del figlio sinistro del nodo `i`. Ritorna un
+   indice non valido se `i` non ha figlio sinistro. */
+static int lchild(const MinHeap *h, int i)
+{
+    assert(valid(h, i));
+
+    return 2*i + 1;
+}
+
+/* Restituisce l'indice del figlio destro del nodo `i`. Ritorna un
+   indice non valido se `i` non ha figlio destro. */
+static int rchild(const MinHeap *h, int i)
+{
+    assert(valid(h, i));
+
+    return 2*i + 2;
+}
+
+/* Restituisce l'indice del figlio di `i` con priorità minima. Se `i`
+   non ha figli, restituisce -1 */
+static int min_child(const MinHeap *h, int i)
+{
+    int l, r, result = -1;
+
+    assert(valid(h, i));
+
+    l = lchild(h, i);
+    r = rchild(h, i);
+    if (valid(h, l)) {
+        result = l;
+        if (valid(h, r) && (h->heap[r].prio < h->heap[l].prio)) {
+            result = r;
+        }
+    }
+    return result;
+}
+
+/* Scambia l'elemento in posizione `i` con il padre fino a quando
+   raggiunge la posizione corretta nello heap */
+static void move_up(MinHeap *h, int i)
+{
+    int p;
+
+    assert(valid(h, i));
+
+    p = parent(h, i);
+    while ( valid(h, p) && (h->heap[i].prio < h->heap[p].prio) ) {
+        swap(h, i, p);
+        i = p;
+        p = parent(h, i);
+    }
+}
+
+/* Scambia l'elemento in posizione `i` con il figlio avente priorità
+   minima, fino a quando l'elemento raggiunge la posizione
+   corretta. Questa funzione corrisponde a Min-Heapify() */
+static void move_down(MinHeap *h, int i)
+{
+    int done = 0;
+
+    assert(valid(h, i));
+
+    /* L'operazione viene implementata iterativamente, sebbene sia
+       possibile una implementazione ricorsiva probabilmente più
+       leggibile. */
+    do {
+        const int dst = min_child(h, i);
+        if (valid(h, dst) && (h->heap[dst].prio < h->heap[i].prio)) {
+            swap(h, i, dst);
+            i = dst;
+        } else {
+            done = 1;
+        }
+    } while (!done);
+}
+
+/* Restituisce true (nonzero) se lo heap è vuoto */
+int minheap_is_empty(const MinHeap *h)
+{
+    assert(h != NULL);
+
+    return (h->n == 0);
+}
+
+/* Restituisce true (nonzero) se lo heap è pieno, cioè è stata
+   esaurita la capienza a disposizione */
+int minheap_is_full(const MinHeap *h)
+{
+    assert(h != NULL);
+
+    return (h->n == h->size);
+}
+
+/* Restituisce il numero di elementi presenti nello heap */
+int minheap_get_n(const MinHeap *h)
+{
+    assert(h != NULL);
+
+    return h->n;
+}
+
+/* Restituisce la chiave associata alla priorità minima */
+int minheap_min(const MinHeap *h)
+{
+    assert( !minheap_is_empty(h) );
+
+    return h->heap[0].key;
+}
+
+/* Come minheap_min(), ma restituisce la coppia (chiave, prio) */
+HeapElem minheap_min2( const MinHeap *h)
+{
+    assert( !minheap_is_empty(h) );
+
+    return h->heap[0];
+}
+
+/* Inserisce una nuova coppia (key, prio) nello heap. */
+void minheap_insert(MinHeap *h, int key, double prio)
+{
+    int i;
+
+    assert( !minheap_is_full(h) );
+    assert((key >= 0) && (key < h->size));
+    assert(h->pos[key] == -1);
+
+    i = h->n++;
+    h->pos[key] = i;
+    h->heap[i].key = key;
+    h->heap[i].prio = prio;
+    move_up(h, i);
+}
+
+/* Rimuove la coppia (chiave, priorità) con priorità minima;
+   restituisce la chiave associata alla priorità minima. */
+int minheap_delete_min(MinHeap *h)
+{
+    int result;
+
+    assert( !minheap_is_empty(h) );
+
+    result = minheap_min(h);
+    swap(h, 0, h->n-1);
+    assert( h->heap[h->n - 1].key == result );
+    h->pos[result] = -1;
+    h->n--;
+    if (!minheap_is_empty(h)) {
+        move_down(h, 0);
+    }
+    return result;
+}
+
+/* Come minheap_delete_min(), ma restituisce la coppia (chiave, prio) */
+HeapElem minheap_delete_min2(MinHeap *h)
+{
+    HeapElem result;
+
+    assert( !minheap_is_empty(h) );
+
+    result = minheap_min2(h);
+    swap(h, 0, h->n-1);
+    assert( h->heap[h->n - 1].key == result.key );
+    h->pos[result.key] = -1;
+    h->n--;
+    if (!minheap_is_empty(h)) {
+        move_down(h, 0);
+    }
+    return result;
+}
+
+/* Modifica la priorità associata alla chiave key. La nuova priorità
+   può essere maggiore, minore o uguale alla precedente. */
+void minheap_change_prio(MinHeap *h, int key, double newprio)
+{
+    int j;
+    double oldprio;
+
+    assert(h != NULL);
+    assert(key >= 0 && key < h->size);
+    
+    j = h->pos[key];
+    assert( valid(h, j) );
+    oldprio = h->heap[j].prio;
+    h->heap[j].prio = newprio;
+    if (newprio > oldprio) {
+        move_down(h, j);
+    } else {
+        move_up(h, j);
+    }
+}
+
+/*--------------FINE MINHEAP---------------*/
+
+/* INIZIO CODICE SUL GRAFO */
+
 /*fine definizione delle strutture*/
 
 /*funzioni per il grafo*/
@@ -172,10 +479,11 @@ int *read_dimension(FILE *filein)
  ***/
 int **load_matrix(FILE *filein, int n, int m)
 {
-    int i = 0, j = 0, skyscapers=0;
+    int i = 0, j = 0;
     int  tmp;
     int delta = INT_MIN;
     int **matrix;
+    int skyscapers=0;
     char c;
     /*inizializzo la matrice con calloc in maniera da pulire le memeorie*/
     matrix = (int **)calloc(n+1, sizeof(int *));
@@ -187,11 +495,7 @@ int **load_matrix(FILE *filein, int n, int m)
             tmp = atoi(&c);
             if (tmp > 0)
             {
-                /*mi serve per poter allocare solo i nodi necessari
-                al grafo*/
                 skyscapers++;
-                /*bug sul delta, un palazzo da 9 piani copre anche le celle 
-                che non coprirebbero palazzi piccoli successivi*/
                 if (tmp+j > delta)
                 {
                     delta = tmp + j <= m ? tmp + j : m;
@@ -220,11 +524,23 @@ int **load_matrix(FILE *filein, int n, int m)
         c = fgetc(filein);
     }
    
-    fclose(filein);
     matrix[n+1] = (int*)malloc(sizeof(int));
     matrix[n+1][0] = skyscapers;
+    fclose(filein);
     /*printf("skyscapers: %d\n", matrix[n+1][0]);*/
     return matrix;
+}
+/*Genera double random sicuramente maggiore del valore affidato per archi entranti in 
+celle asciutte */
+static double generate_random_number()
+{
+    double random_number; 
+    srand(time(NULL));
+    random_number = ((double)rand() / (double)RAND_MAX) * 100.0;
+    while (random_number <= 3.0) {
+        random_number = ((double)rand() / (double)RAND_MAX) * 100.0;
+    }
+    return random_number;
 }
 /***
  * Edoardo Desiderio.
@@ -241,10 +557,8 @@ int **load_matrix(FILE *filein, int n, int m)
 void fill_graph(Graph* g,int** matrix, int n,int m){
     
     int i,j;
-    double wet_weigth;
-    srand(time(NULL));
-    wet_weigth = (double)rand() / (double)RAND_MAX;
-
+    double wet_weigth = generate_random_number();
+    
     for (i = 0; i < n; i++)
     {
         for (j = 0; j < m; j++)
@@ -264,6 +578,102 @@ void fill_graph(Graph* g,int** matrix, int n,int m){
     }
 }
 
+/*FINE CODICE SUL GRAFO*/
+
+/*algoritmo di dykstra*/
+void dijkstra( const Graph *g, int s, double *d, int *p, const Edge **sp)
+{
+    int i;
+    int v,u;
+    const Edge *e;
+
+    /*inizializzazione contenuti*/
+    for ( i = 0; i < g->n; i++)
+    {
+        sp[i]=NULL;
+        d[i] = HUGE_VAL;
+        p[i] = NODE_UNDEF;
+    }
+    d[s] = 0 ;
+    p[s] = NODE_UNDEF;
+    /*inizializzazione coda di priorità*/
+    Q = minheap_create(g->n);
+
+    
+    for ( i = 0; i < g->n; i++)
+    {
+        minheap_insert(Q, i, d[i]);
+    }
+
+    /*ciclo principale*/
+    while (!minheap_is_empty(Q))
+    {
+        u = minheap_delete_min(Q);
+        e = graph_adj(g, u);
+        while (e != NULL)
+        {
+            v = e->dst;
+            if (d[v] > d[u] + e->weight)
+            {
+                d[v] = d[u] + e->weight;
+                p[v] = u;
+                sp[v] = e;
+                minheap_change_prio(Q,v,d[v]);
+            }
+            e = e->next;
+        }
+    }
+    /*beauty print array Q->pos*/
+}
+void print_path(const int *p, int src, int dst)
+{
+    if (src == dst)
+    {
+        printf("%d",src);
+    }
+    else if (p[dst] == NODE_UNDEF)
+    {
+        printf("Non raggiungibile");
+    }
+    else
+    {
+        print_path(p,src,p[dst]);
+        printf("->%d",dst);
+    }
+    
+}
+void print_dist( const Graph *g, int src, int dst, const int *p, const double *d )
+{
+    const int n = graph_n_nodes(g);
+    int v, from_idx = 0, to_idx = n-1;
+
+    assert(p != NULL);
+    assert(d != NULL);
+
+    if (dst >= 0) {
+        from_idx = to_idx = dst;
+    }
+
+    printf(" src  dst            d path\n");
+    printf("---- ---- ------------ -------------------------\n");
+    for (v=from_idx; v<=to_idx; v++) {
+        printf("%4d %4d %12.4f ", src, v, d[v]);
+        print_path(p, src, v);
+        printf("\n");
+    }
+
+}
+
+void matrix_destroy(int **array, int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        free(array[i]);
+    }
+    
+}
+/**/
 /***
  * @author Edoardo Desiderio
  * entry point del programma, avvia le varie funzioni
@@ -276,10 +686,16 @@ void fill_graph(Graph* g,int** matrix, int n,int m){
 int main(int argc, char const *argv[])
 {
     int *m_n;
-    int m, n;
-    int skyscapers;
+    int m, n,skyscapers;
     int **matrix;
     FILE *filein = stdin;
+    const Edge **sp; /* sp[v] è il puntatore all'arco che collega v
+                        con il suo predecessore nell'albero dei
+                        cammini minimi */
+    double *d;       /* d[v] è la distanza minima dalla sorgente al
+                        nodo v */
+    int *p;         /* p[v] è il predecessore di v nel cammino
+                        minimo dalla sorgente */
     Graph *g;
     if (argc != 2)
     {
@@ -306,11 +722,20 @@ int main(int argc, char const *argv[])
     matrix = load_matrix(filein, n, m);
     skyscapers = matrix[n+1][0];
     free(matrix[n+1]);
-
-    
-    g = graph_create(n * m - skyscapers);
+    printf("skyscapers: %d\n", skyscapers);
+    g = graph_create(n * m);
     assert(g != NULL);
     fill_graph(g, matrix, n, m);
     graph_print(g);
+    matrix_destroy(matrix, n);
+   
+    
+    
+    d = (double*)malloc(n * sizeof(*d)); assert(d != NULL);
+    p = (int*)malloc(n * sizeof(*p)); assert(p != NULL);
+    sp = (const Edge**)malloc(n * sizeof(*sp)); assert(sp != NULL);
+
+    dijkstra(g, 0, d, p, sp);
+    print_path(p,0,GRAPH_INDEX(n-1,m-1,m));
     return EXIT_SUCCESS;
 }
